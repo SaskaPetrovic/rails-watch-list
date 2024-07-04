d16fb49e7a3f40d40d61c9d6491c58530c1e966d
saska petrovic <saskapetrovic.pro@gmail.com>
2024-07-04 17:01:03 +0200

    attempt 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Creating movies..."
+
+10.times do
+  movie = Movie.create!(
+    title: Faker::Movie.unique.title,
+    overview: Faker::Lorem.paragraph(sentence_count: 3),
+    poster_url: Faker::Internet.url,
+    rating: rand(0.0..10.0).round(1)
+  )
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
